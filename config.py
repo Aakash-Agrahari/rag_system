cec4a6532687f695e838e4a1af013034e08dca16
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-07-23 23:57:48 +0530

    initial rag

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+DOCUMENTS_PATH = "documents"
+CHROMA_PATH = "chroma_db"
+COLLECTION_NAME = "notes"
